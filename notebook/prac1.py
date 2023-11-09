df400f41b39fb39aa042c44e94b73fba963391f2
m4r4d0n4 <juansebasmontes@gmail.com>
2023-11-09 21:55:25 +0100

    sss

--- notebook/prac1.py
+++ notebook/prac1.py
@@ -32,7 +32,54 @@ from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, RandomForest
 from sklearn.metrics import confusion_matrix, precision_recall_curve, roc_curve
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures
 from sklearn.manifold import LocallyLinearEmbedding, Isomap, TSNE
+from datetime import datetime
+import csv
+#UTILS
+def describe_pipeline(pipeline):
+    """
+    Función para describir las funciones aplicadas en cada etapa de un pipeline de Scikit-Learn.
 
+    Args:
+    pipeline (Pipeline): El pipeline de Scikit-Learn a describir.
+
+    Returns:
+    str: Una cadena de texto describiendo las funciones aplicadas en cada etapa del pipeline.
+    """
+    steps = pipeline.steps
+    
+    descriptions = []
+    for step_name, step_object in steps:
+        if isinstance(step_object, Pipeline):
+            # Si el paso es un pipeline, describe recursivamente el subpipeline
+            subpipeline_description = describe_pipeline(step_object)
+            descriptions.append(f"{step_name}:\n{subpipeline_description}")
+        else:
+            if isinstance(step_object, BaseEstimator):
+                descriptions.append(f"{step_name}: {type(step_object).__name__}")
+            else:
+                descriptions.append(f"{step_name}: Custom Function")
+    
+    return '\n'.join(descriptions)
+
+
+def guardar_en_csv(texto, accuracy):
+    # Obtener la fecha y hora actual
+    fecha_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # Eliminar saltos de línea del texto
+    texto = texto.replace('\n', ' ')
+    # Datos a guardar en el archivo CSV
+    data = [[fecha_actual,texto, accuracy ]]
+
+    # Nombre del archivo CSV
+    nombre_archivo = 'metricas_obtenidas.csv'
+
+    # Escribir los datos en el archivo CSV
+    with open(nombre_archivo, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        # Si es la primera vez que se escribe en el archivo, se escribe el encabezado
+        if file.tell() == 0:
+            writer.writerow(['Fecha y Hora','Texto', 'Accuracy' ])
+        writer.writerows(data)
 
 def lecturaDatos(path_train:str, path_tags:str):   
 
@@ -91,13 +138,16 @@ if __name__ == "__main__":
 
     # Definimos el espacio de búsqueda de hiperparámetros
     param_grid = {
-        'model__max_depth': [1,2,3],  
-        'model__n_estimators': [10,100,1000],  
-        'model__learning_rate': [0.01, 0.1, 1]  
+        'model__max_depth': [1,2],  
+        'model__n_estimators': [10],  
+        'model__learning_rate': [ 0.01,0.1,1 ]  
     }
+    # Crear un objeto KFold para especificar la estrategia K-fold
+    #kf = KFold(n_splits=5, shuffle=True, random_state=random_state)  # Puedes ajustar n_splits según tu preferencia
 
     # Creamos un objeto GridSearchCV para el pipeline
-    grid_search = GridSearchCV(pipeline, param_grid, cv=5)
+
+    grid_search = GridSearchCV(pipeline, param_grid, cv=5,verbose=2) #usar cv=kf
     # Realizamos la búsqueda de hiperparámetros utilizando los datos de entrenamiento
     grid_search.fit(X_train, y_train)
     # Mostramos los mejores hiperparámetros encontrados
@@ -108,6 +158,7 @@ if __name__ == "__main__":
     best_model = grid_search.best_estimator_
     accuracy = best_model.score(X_test, y_test)
     print(f"Precisión del modelo con los mejores hiperparámetros en datos de prueba: {accuracy:.2f}")
+    guardar_en_csv(describe_pipeline(pipeline),accuracy=accuracy)
 
 
 
