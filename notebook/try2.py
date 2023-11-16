ae2277bc8908d9f0d90c4625d6c6f10079fb65b7
hanaiga <78901053+hanaiga@users.noreply.github.com>
2023-11-16 17:52:50 +0100

    sasa

--- notebook/try2.py
+++ notebook/try2.py
@@ -2,6 +2,7 @@
 import random
 import numpy as np
 import pandas as pd
+
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV, train_test_split, cross_validate
 from sklearn.feature_selection import VarianceThreshold
