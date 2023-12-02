80b99d76922154cc2a4a400df1958277b481b8b0
m4r4d0n4 <juansebasmontes@gmail.com>
2023-12-03 00:48:43 +0100

    reorden and finishing oc

--- train_model.py
+++ train_model.py
@@ -101,7 +101,7 @@ def guardar_en_csv(texto, params ,accuracy):
             writer.writerow(['Fecha y Hora','Texto', 'Accuracy' ])
         writer.writerows(data)
 
-def lecturaDatos(path_train:str, path_tags:str):   
+def lecturaDatos(path_train:str):   
 
     #Leemos el CSV de entrenamiento
     X_init = pd.read_csv(path_train, sep = ',', decimal = '.', index_col=0)
@@ -162,7 +162,7 @@ def normalizing_and_reducing()->Pipeline:
     return Pipeline(filters_features)
 
 # Función para aumentar caracteristicas
-def feature_augmentation(X):
+def log_aumentation(X):
     """
     Realiza aumento de características
 
@@ -175,21 +175,63 @@ def feature_augmentation(X):
     # Aplicar logaritmo a las características
     log_transformed_data = np.log1p(X)
 
-    # Aumento de características (puedes personalizar esta parte según tus necesidades)
-    #squared_data = np.square(X) este me viene dado por polyfeatures ya
-    #cubed_data = np.power(X, 3)
-    #sqrt_data = np.sqrt(np.abs(X))
+    # Concatenar las características originales con las nuevas características aumentadas
+    augmented_data = np.concatenate((X,log_transformed_data), axis=1) #Si se descomenta algun añadido de datos tenemos que concatenarlo aquí
 
-    # Binneamos
-    # Aplicar agrupamiento a cada característica
-    #binned_data = np.apply_along_axis(lambda x: np.digitize(x, np.linspace(min(x), max(x), num=5)), axis=0, arr=X)
+    return augmented_data
 
-    # Derivadas
-    #diff_data = np.diff(X, axis=1)
-    #derivative_data = np.gradient(X, axis=1)
+# Función para aumentar caracteristicas
+def cubed_aumentation(X):
+    """
+    Realiza aumento de características
+
+    Parametros:
+    - data: numpy array o pandas DataFrame, el conjunto de datos original.
+
+    Devuelve:
+    - augmented_data: numpy array o pandas DataFrame, el conjunto de datos aumentado.
+    """
+
+    # Aumento de características
+    cubed_data = np.power(X, 3)
+    augmented_data = np.concatenate((X,cubed_data), axis=1) 
+
+    return augmented_data
+# Función para aumentar caracteristicas
+def sqrt_aumentation(X):
+    """
+    Realiza aumento de características
+
+    Parametros:
+    - data: numpy array o pandas DataFrame, el conjunto de datos original.
+
+    Devuelve:
+    - augmented_data: numpy array o pandas DataFrame, el conjunto de datos aumentado.
+    """
+    
+    sqrt_data = np.sqrt(np.abs(X))
 
     # Concatenar las características originales con las nuevas características aumentadas
-    augmented_data = np.concatenate((X,log_transformed_data), axis=1)
+    augmented_data = np.concatenate((X,sqrt_data), axis=1) 
+
+    return augmented_data
+# Función para aumentar caracteristicas
+def bin_aumentation(X):
+    """
+    Realiza aumento de características
+
+    Parametros:
+    - data: numpy array o pandas DataFrame, el conjunto de datos original.
+
+    Devuelve:
+    - augmented_data: numpy array o pandas DataFrame, el conjunto de datos aumentado.
+    """
+    # Aplicar agrupamiento a cada característica
+    binned_data = np.apply_along_axis(lambda x: np.digitize(x, np.linspace(min(x), max(x), num=5)), axis=0, arr=X)
+
+
+    # Concatenar las características originales con las nuevas características aumentadas
+    augmented_data = np.concatenate((X,binned_data), axis=1) #Si se descomenta algun añadido de datos tenemos que concatenarlo aquí
 
     return augmented_data
 
@@ -199,13 +241,20 @@ def addingFeatures()->Pipeline:
     polyfeat = PolynomialFeatures().set_output(transform="pandas")
     more_features.append( ('addPolyFeat', polyfeat) )
     
-    #Aumentamos caracteristicas (esta comentado porque todos los aumetnados de caracteristicas que hemos probado, salvo el Poly nos reducen la precisión)
-    #feature_transformer = FunctionTransformer(feature_augmentation) 
-    #more_features.append( ('feature_augmentation', feature_transformer) )
+    #Aumentamos caracteristicas (esta comentado porque todos los aumetnados de caracteristicas que hemos probado, salvo el Poly, nos reducen la accuracy)
+    '''
+    log_aumentation_pipeline = FunctionTransformer(log_aumentation) 
+    more_features.append( ('log_augmentation', log_aumentation_pipeline) )
+
+    cubed_aumentation_pipeline = FunctionTransformer(cubed_aumentation) 
+    more_features.append( ('cubed_augmentation', cubed_aumentation_pipeline) )
     
-    # Definir el imputador KNN (regulariza los datos mucho y no es deseable, solo lo usamos si nos da NaN algun aumentado de caracteristicas)
-    #imputer = KNNImputer()
-    #more_features.append(('nan_imputer',imputer))
+    sqrt_aumentation_pipeline = FunctionTransformer(sqrt_aumentation) 
+    more_features.append( ('sqrt_augmentation', sqrt_aumentation_pipeline) )
+
+    bin_aumentation_pipeline = FunctionTransformer(bin_aumentation) 
+    more_features.append( ('bin_augmentation', bin_aumentation_pipeline) )
+'''
 
     #Operaciones en paralelo
     return FeatureUnion(more_features)
@@ -239,50 +288,45 @@ def grid_testing(pipeline,params,X,y,X_test,y_test):
     #Guardamos el modelo para no perder el entrenamiento
     joblib.dump(grid_search.best_estimator_, 'modelo_guardado.joblib')
 
+    #Guardamos la metrica
     try:
         guardar_en_csv(describe_pipeline(pipeline),grid_search.best_params_,accuracy=grid_search.best_estimator_.score(X_test, y_test))
     except:
         guardar_en_csv("error_descripcion_pipeline",grid_search.best_params_,accuracy=grid_search.best_estimator_.score(X_test, y_test))
     
     #Devolvemos el modelo entrenado
-
     return grid_search.best_estimator_
 
 
 #Ejecución del código
 if __name__ == "__main__":
    
-    #Lectura de los datos
-    X1 = lecturaDatos("../traintabs/traintab01.csv")
-    
-    X2 = lecturaDatos("../traintabs/traintab02.csv")
+    #GENERAMOS EL FICHERO COMPETICION DE METEMOS LOS PATHS
+    X = [lecturaDatos(i) for i in ["./traintabs/traintab01.csv",
+                                                     "./traintabs/traintab02.csv",
+                                                     "./traintabs/traintab03.csv",
+                                                     "./traintabs/traintab04.csv",
+                                                     "./traintabs/traintab05.csv",
+                                                     "./traintabs/traintab06.csv",
+                                                     "./traintabs/traintab07.csv",
+                                                     "./traintabs/traintab08.csv"
+                                                     ]
+    ]
 
-    X3 = lecturaDatos("../traintabs/traintab03.csv")
-
-    X4 = lecturaDatos("../traintabs/traintab04.csv")
-
-    X5 = lecturaDatos("../traintabs/traintab05.csv")
-
-    X6 = lecturaDatos("../traintabs/traintab06.csv")
-
-    X7 = lecturaDatos("../traintabs/traintab07.csv")
-
-    X8 = lecturaDatos("../traintabs/traintab08.csv")
-
-    X = pd.concat([X1,X2,X3,X4,X5,X6,X7,X8],axis=1)
+    X = pd.concat(X,axis=1)
 
     # Dividimos los datos en conjuntos de entrenamiento y prueba
     # Porcentaje de la muestra reservada para testear
     test_size = 0.2
     # Semilla
     random_state = 1338
-    y = lecturaTags("../traintabs/train_label.csv")
+    y = lecturaTags("./traintabs/train_label.csv")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
 
     ###CLASIFICADOR ELEGIDO
     # Opciones
-    option = 'bagging' # 'voting','svm','adaboost','rf'
+    option = 'svm' # 'voting','svm','adaboost','rf'
 
     if option == 'voting':
         # Definir el modelo de votación
@@ -299,8 +343,8 @@ if __name__ == "__main__":
         ###HIPERPARAMETROS
         params_grid = { 
             # Hiperparámetros SVC
-            'model__svm__kernel': ['rbf'],  #5 y 0.1  
-            'model__svm__C' : [0,1,1,10],
+            'model__svm__kernel': ['rbf'],  # 'linear', 'poly'
+            'model__svm__C' : [0,1,1,10], #C para el slack (mayor C mas penaliza el slack)
             # Hiperparámetros DT
             'model__dt__min_samples_split': [2,4],
             'model__dt__min_samples_leaf': [1,2],
@@ -316,20 +360,19 @@ if __name__ == "__main__":
         params_grid = {
             # Hiperparámetros SVC
             'model__estimator__kernel': ['rbf'],  # 'linear', 'poly' 
-            'model__estimator__C' : [0.01,0.5,1,10], #Ajustamos cuanto afecta el Slack
+            'model__estimator__C' : [0.01,0.5,1,10], #C para el slack (mayor C mas penaliza el slack)
             # Hiperparámetros Bagging
             'model__n_estimators': [20,50,100] #Cuantos estimadores creamos
 
         }
-    elif option == 'SVM':
+    elif option == 'svm':
         #Clasificador SVM, seleccionamos el kernel al ajustar hiperparametros
         clf = SVC(cache_size=4000,probability=True, random_state=1337)
 
         ####HIPERPARÁMETROS
         params_grid = { 
-            # Hiperparámetros SVC
-            'model__kernel': ['linear'],  #5 y 0.1  
-            'model__C' : [0.05],  #5 y 0.1  
+            'model__kernel': ['linear'],  #'linear', 'poly'  
+            'model__C' : [0.05],  #C para el slack (mayor C mas penaliza el slack)  
         }
     elif option=='rf':
         clf = RandomForestClassifier()
@@ -362,18 +405,18 @@ if __name__ == "__main__":
 
 
     #GENERAMOS EL FICHERO COMPETICION DE METEMOS LOS PATHS
-    data = [lecturaDatos(i) for i in ["../testtabs/testtab01.csv",
-                                                     "../testtabs/testtab02.csv",
-                                                     "../testtabs/testtab03.csv",
-                                                     "../testtabs/testtab04.csv",
-                                                     "../testtabs/testtab05.csv",
-                                                     "../testtabs/testtab06.csv",
-                                                     "../testtabs/testtab07.csv",
-                                                     "../testtabs/testtab08.csv"
+    data = [lecturaDatos(i) for i in ["./testtabs/testtab01.csv",
+                                                     "./testtabs/testtab02.csv",
+                                                     "./testtabs/testtab03.csv",
+                                                     "./testtabs/testtab04.csv",
+                                                     "./testtabs/testtab05.csv",
+                                                     "./testtabs/testtab06.csv",
+                                                     "./testtabs/testtab07.csv",
+                                                     "./testtabs/testtab08.csv"
                                                      ]
             ]
     TEST = pd.concat(data,axis=1)
-    y_pred = modelo.preditc(TEST)
+    y_pred = modelo.predict(TEST)
     np.savetxt('Competicion1.txt', y_pred, fmt='%i', delimiter=',')
 
 
